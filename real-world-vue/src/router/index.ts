a0d2c275af16510715af160e9b3b2561049acb3e
Thanakorn Waleejaroenpong <tanakorn_wal@cmu.ac.th>
2023-07-18 14:01:22 +0700

    1.11

---
+++ real-world-vue/src/router/index.ts
@@ -0,0 +1,40 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import EventListView from '../views/EventListView.vue'
+import AboutView from '../views/AboutView.vue'
+import CategoryView from '../views/CategoryView.vue'
+import StudentView from '../views/StudentView.vue'
+import EventDetailView from '../views/EventDetailView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'EventList',
+      component: EventListView
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: AboutView
+    },
+    {
+      path: '/category',
+      name: 'category',
+      component: CategoryView
+    },
+    {
+      path: '/student',
+      name: 'student',
+      component: StudentView
+    },
+    {
+      path: '/event/:id',
+      name: 'event-detail',
+      component: EventDetailView,
+      props: true
+    }
+  ]
+})
+
+export default router
